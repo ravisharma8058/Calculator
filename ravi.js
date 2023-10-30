676ecab6c2e08d98c1fe6584f2b80aeae0f12c98
ravisharma8058 <sharmaravi252002@gmail.com>
2023-10-30 23:24:58 +0530

    first commit

---
+++ ravi.js
@@ -0,0 +1,33 @@
+// var x = document.getElementById("screen")
+// x.addEventListener("click",function(){
+    
+//     alert("hello you are in text");
+
+// })
+ let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'Ac') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
+
